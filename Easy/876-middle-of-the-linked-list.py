dfd990a9df9ccbe1374c329559733a27af2731a1
ohussa <oyhussai@asu.edu>
2025-05-14 15:08:15 +0800

    method2

--- Easy/876-middle-of-the-linked-list.py
+++ Easy/876-middle-of-the-linked-list.py
@@ -1,21 +1,23 @@
 Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
->>> # Definition for singly-linked list.
-... # class ListNode(object):
-... #     def __init__(self, val=0, next=None):
-... #         self.val = val
-... #         self.next = next
-... 
-... 
-... """Problem Description:Given the head of a singly linked list, return the middle node of the linked list.
-... 
-... If there are two middle nodes, return the second middle node."""
-... class Solution(object):
-...     def middleNode(self, head):
-...         """
-...         :type head: Optional[ListNode]
-...         :rtype: Optional[ListNode]
-...         """
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+
+"""Problem Description:Given the head of a singly linked list, return the middle node of the linked list.
+
+If there are two middle nodes, return the second middle node."""
+class Solution(object):
+    def middleNode(self, head):
+        """
+        :type head: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        """
+...         Method1:
 ...         current=head
 ...         counter=0
 ...         while current.next:
@@ -36,9 +38,21 @@ Type "help", "copyright", "credits" or "license()" for more information.
 ...         #i first go till end and find number of nodes
 ...         #then len//2 add 1. This works for where 2 middle or one both
 ...         #then traverse again this time till middle node
-        #return middle node.
-        #time complexity becomes O(n+0.5n)
-
-        #Method2
-        #store each node in arraylist
-        #after we get the legnth of linkedlist
+...         #return middle node.
+...         #time complexity becomes O(n+0.5n)"""
+... 
+...         #Method2
+...         #store each node in list
+...         #after we get the legnth of linkedlist
+...         #get the middle node
+...         current=head
+...         counter=0
+...         nodes_list=[]
+...         #while current is not null,on last node since when do 
+...         #current=current.next,so loop wont run again,since current.next was null
+...         while current:
+...             nodes_list.append(current)
+...             current=current.next
+...         mid_node=(len(nodes_list)//2)
+...         print(nodes_list[-1])
+...         return nodes_list[mid_node]
