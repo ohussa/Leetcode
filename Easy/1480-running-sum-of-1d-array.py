729a6036a98bd46bc6741244df01cdb4614a8376
ohussa <oyhussai@asu.edu>
2025-05-14 14:26:54 +0800

    Create 1480-running-sum-of-1d-array.py

---
+++ Easy/1480-running-sum-of-1d-array.py
@@ -0,0 +1,14 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> class Solution(object):
+...     def runningSum(self, nums):
+...         """
+...         :type nums: List[int]
+...         :rtype: List[int]
+...         """
+...         runningSums=nums
+...         for i in range(len(runningSums)):
+...             if i==0:
+...                 runningSums[i]=runningSums[i]
+...             else:
+...                 runningSums[i]=runningSums[i-1]+runningSums[i]
